2405b7a9d73b2d9fe664980e65f6df636e8e7af9
spomni <mikhail.snitko@gmail.com>
2019-03-13 06:26:56 +0300

    feat: Implement the function makeExchange().

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  if (currency > 10000){
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  }
+  
+  if (currency <= 0){
+    return {}
+  }
+  
+  let config = [
+    ["H", 50],
+    ["Q", 25],
+    ["D", 10],
+    ["N", 5],
+    ["P", 1]
+  ];
+  
+  let res = {};
+  
+  let length = config.length;
+  for (let i=0; i<length; i++){
+  
+    if (currency === 0){
+      return res;
+    }
+  
+    let coin = config[i][0];
+    let volume = config[i][1];
+    
+    let count = (currency/volume) | 0;
+    
+    if (count !== 0){
+      res[coin] = count;
+      currency = currency - count * volume;
+    }
+  }
+  
+  return res;
 }
